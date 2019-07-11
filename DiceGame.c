bf8348ea43cca7998ed526f89b1b39799620eb34
s1260095 <s1260095@u-aizu.ac.jp>
2019-07-11 18:22:52 +0900

    commit3

--- DiceGame.c
+++ DiceGame.c
@@ -3,6 +3,10 @@
 #include<time.h>
 int main(){
   int sum,num1,num2;
+  char name[256];
+  printf("What is your name?\n");
+  scanf("%s",name);
+  printf("Hello, %s\n",name);
   printf("Rolling the dice\n");
   srand(time(NULL));
   num1=rand()%6+1;
@@ -11,5 +15,9 @@ int main(){
   printf("Die 1:%d\n",num1);
   printf("Die 2:%d\n",num2);
   printf("Total value: %d\n",sum);
+  if(sum>6)
+    printf("You won\n");
+  else
+    printf("You lost\n");
   return 0;
 }
