85cacaa85392e2490e43a4d4bacc19e34e4b0333
s1260095 <s1260095@u-aizu.ac.jp>
2019-07-11 18:17:18 +0900

    commit1

---
+++ DiceGame.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main(){
+  int sum,num1,num2;
+  printf("Rolling the dice\n");
+  srand(time(NULL));
+  num1=rand()%6+1;
+  num2=rand()%6+1;
+  sum=num1+num2;
+  printf("Die 1:%d\n",num1);
+  printf("Die 2:%d\n",num2);
+  printf("Total value: %d\n",sum);
+  return 0;
+}
